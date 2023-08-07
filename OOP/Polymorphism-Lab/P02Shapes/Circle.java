73530132de67d9afa3bf8c8c45d07bad9ab4e01b
Aysssi <ayselchausheva@abv.bg>
2023-08-07 16:09:21 +0300

    update

---
+++ OOP/Polymorphism-Lab/P02Shapes/Circle.java
@@ -0,0 +1,29 @@
+package LabPolymorphism.P02Shapes;
+
+public class Circle extends Shape{
+    private Double radius;
+
+    public Circle(Double radius) {
+        this.setRadius(radius);
+        this.calculatePerimeter();
+        this.calculateArea();
+    }
+
+    public final Double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    protected void calculatePerimeter() {
+        setPerimeter(this.radius * 2 * Math.PI);
+    }
+
+    @Override
+    protected void calculateArea() {
+        setArea(Math.pow(this.radius, 2) * Math.PI);
+    }
+}
